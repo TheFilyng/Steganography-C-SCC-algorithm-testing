3dc89e28b37755554619b0f900bc6a5aea3fbaf6
Agustin Barruti <agustinbarruti@gmail.com>
2022-03-21 17:56:01 +0100

    Creado StegManager con implementación inicial de SCC.

--- StegManager.cs
+++ StegManager.cs
@@ -1,47 +1,73 @@
 using System;
 using System.Drawing;
+using System.Text;
+using System.Text.Json;
 
-namespace Test_Steg
+namespace TestSteg
 {
     public class StegManager
     {
 
-        String mensaje;
-        Color[] pixels;
-
-        public StegManager()
+        private RunData data;
+        private Color[] pixels;
+        private ImageManager manager;
+        public StegManager(RunData data, Image coverImage)
         {
-            this.mensaje = "";
-            pixels = new Color[0];
-        }
-        public StegManager(String mensaje)
-        {
-            this.mensaje = mensaje;
-            pixels = new Color[0];
-        }
-
-        public void setMensaje(String mensaje){
-            this.mensaje = mensaje;
-        }
-
-        public void codificarMensaje(Image coverImage)
-        {
-            ImageManager manager = new ImageManager(coverImage);
-            byte R;
-            byte G;
-            byte B;
+            this.data = data;
+            manager = new ImageManager(coverImage);
             pixels = manager.getPixels();
-            for (int i = 0; i < pixels.Length; i++)
-            {
-                //Obtenemos cada canal (byte) del pixel
-                R = pixels[i].R;
-                G = pixels[i].G;
-                B = pixels[i].B;
+            Console.WriteLine("Creado StegManager");
+        }
 
-                //TODO: Convertir mensaje a codificar a bits
-                //TODO: Realizar algoritmo de SCC
-                
+        private String generarBitsdeJson()
+        {
+            String mensaje = JsonSerializer.Serialize(data);
+            Byte[] mensajeEnBytes = Encoding.Unicode.GetBytes(mensaje);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in mensajeEnBytes)
+            {
+                sb.Append(Convert.ToString(b, 2));
             }
+
+            return sb.ToString();
+        }
+
+        public void codificarMensaje()
+        {
+            int R;
+            int G;
+            int B;
+
+            String bitsMensaje = generarBitsdeJson();
+            StringBuilder sb = new StringBuilder(bitsMensaje);
+            int index = 0;
+            Console.WriteLine("Comienzo generación de stego image");
+            while(true)
+            {   
+                //Codificar en R 
+                if (sb.Length == index) break;
+                R = pixels[index].R;
+                R = sb[index].Equals('1') ? R | 1 : R & ~1; 
+                index++;             
+                
+                //Codificar en G
+                if (sb.Length == index) break;
+                G = pixels[index].G;
+                G = sb[index].Equals('1') ? G | 1 : G & ~1;
+                index++;
+
+                //Codificar en B
+                if (sb.Length == index) break;
+                B = pixels[index].B;
+                B = sb[index].Equals('1') ? B | 1 : B & ~1;
+                index++;
+            }
+            Console.WriteLine("Fin generación de stego image");
+        }
+
+        public void crearStegoImage()
+        {
+            manager.SaveNewImage(pixels);
         }
     }
 }
